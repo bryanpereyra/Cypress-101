b093ecf29d84fa05ca0abafb8c20c0ed99ac4d8a
Bryan Pereyra <bryanqp0407@gmail.com>
2023-07-08 18:53:49 -0600

    First Commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    // Base Url
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
